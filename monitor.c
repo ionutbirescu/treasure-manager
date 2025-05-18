81fe5ef40bfc2b1104f76d9513bf5a2b23cc207f
Ionuț Birescu (biri) <ionut.birescu55@gmail.com>
2025-05-18 11:06:24 -0400

    modifies monitor.c and treasure_hub.c

--- monitor.c
+++ monitor.c
@@ -11,164 +11,125 @@
 int terminate_flag = 0;
 int process_command_flag = 0;
 
-void handle_sigusr1(int sig)
-{
+void handle_sigusr1(int sig) {
     process_command_flag = 1;
 }
 
-void handle_sigusr2(int sig)
-{
+void handle_sigusr2(int sig) {
     terminate_flag = 1;
 }
 
-int count_treasures(const char *hunt_dir)
-{
+int count_treasures(const char *hunt_dir) {
     char treasure_path[256];
     snprintf(treasure_path, sizeof(treasure_path), "%s/treasures.dat", hunt_dir);
     int fd = open(treasure_path, O_RDONLY);
-    if (fd == -1)
-        return 0;
+    if (fd == -1) return 0;
 
     int count = 0;
     Treasure t;
-    while (read(fd, &t, sizeof(Treasure)) == sizeof(Treasure))
-    {
+    while (read(fd, &t, sizeof(Treasure)) == sizeof(Treasure)) {
         count++;
     }
-
     close(fd);
     return count;
 }
 
-void list_hunts(int pipe_fd)
-{
+void list_hunts() {
     DIR *dir = opendir(".");
-    if (!dir)
-    {
+    if (!dir) {
         perror("Failed to open current directory");
         return;
     }
 
     char buffer[512];
     struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL)
-    {
+    while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR &&
             strcmp(entry->d_name, ".") != 0 &&
             strcmp(entry->d_name, "..") != 0 &&
-            strncmp(entry->d_name, "Hunt", 4) == 0)
-        {
+            strncmp(entry->d_name, "Hunt", 4) == 0) {
             int count = count_treasures(entry->d_name);
             snprintf(buffer, sizeof(buffer), "%s: %d treasures\n", entry->d_name, count);
-            write(pipe_fd, buffer, strlen(buffer));
+            write(STDOUT_FILENO, buffer, strlen(buffer));
         }
     }
-
     closedir(dir);
 }
 
-void monitor_loop()
-{
-    while (!terminate_flag)
-    {
-        if (process_command_flag)
-        {
+void monitor_loop() {
+    while (!terminate_flag) {
+        if (process_command_flag) {
             process_command_flag = 0;
 
             FILE *cmd_file = fopen(".hub_cmd", "r");
-            if (!cmd_file)
-            {
+            if (!cmd_file) {
                 perror("Cannot open .hub_cmd");
                 continue;
             }
 
             char command[256];
-            if (fgets(command, sizeof(command), cmd_file))
-            {
-                command[strcspn(command, "\n")] = '\0'; // remove newline
+            if (fgets(command, sizeof(command), cmd_file)) {
+                command[strcspn(command, "\n")] = '\0'; // strip newline
 
-                int pipe_fd = STDOUT_FILENO;
-                FILE *pf = fopen(".pipe_fd", "r");
-                if (pf)
-                {
-                    fscanf(pf, "%d", &pipe_fd);
-                    fclose(pf);
+                if (strncmp(command, "list_hunts", 10) == 0) {
+                    list_hunts();
                 }
-
-                if (strncmp(command, "list_hunts", 10) == 0)
-                {
-                    list_hunts(pipe_fd);
-                }
-                else if (strncmp(command, "list_treasures", 14) == 0)
-                {
+                else if (strncmp(command, "list_treasures", 14) == 0) {
                     char hunt[128];
-                    if (sscanf(command, "list_treasures %s", hunt) == 1)
-                    {
+                    if (sscanf(command, "list_treasures %s", hunt) == 1) {
                         char buffer[512];
                         snprintf(buffer, sizeof(buffer), "./treasure_manager --list %s", hunt);
                         FILE *fp = popen(buffer, "r");
-                        if (fp)
-                        {
+                        if (fp) {
                             char line[256];
-                            while (fgets(line, sizeof(line), fp))
-                            {
-                                write(pipe_fd, line, strlen(line));
+                            while (fgets(line, sizeof(line), fp)) {
+                                write(STDOUT_FILENO, line, strlen(line));
                             }
                             pclose(fp);
                         }
                     }
                 }
-                else if (strncmp(command, "view_treasure", 13) == 0)
-                {
+                else if (strncmp(command, "view_treasure", 13) == 0) {
                     char hunt[128];
                     int id;
-                    if (sscanf(command, "view_treasure %s %d", hunt, &id) == 2)
-                    {
+                    if (sscanf(command, "view_treasure %s %d", hunt, &id) == 2) {
                         char buffer[512];
                         snprintf(buffer, sizeof(buffer), "./treasure_manager --view %s %d", hunt, id);
                         FILE *fp = popen(buffer, "r");
-                        if (fp)
-                        {
+                        if (fp) {
                             char line[256];
-                            while (fgets(line, sizeof(line), fp))
-                            {
-                                write(pipe_fd, line, strlen(line));
+                            while (fgets(line, sizeof(line), fp)) {
+                                write(STDOUT_FILENO, line, strlen(line));
                             }
                             pclose(fp);
                         }
                     }
                 }
-                else if (strncmp(command, "calculate_score", 15) == 0)
-                {
+                else if (strncmp(command, "calculate_score", 15) == 0) {
                     char hunt[128];
-                    if (sscanf(command, "calculate_score %s", hunt) == 1)
-                    {
+                    if (sscanf(command, "calculate_score %s", hunt) == 1) {
                         char buffer[512];
                         snprintf(buffer, sizeof(buffer), "./calculate_score %s", hunt);
                         FILE *fp = popen(buffer, "r");
-                        if (fp)
-                        {
+                        if (fp) {
                             char line[256];
-                            while (fgets(line, sizeof(line), fp))
-                            {
-                                write(pipe_fd, line, strlen(line));
+                            while (fgets(line, sizeof(line), fp)) {
+                                write(STDOUT_FILENO, line, strlen(line));
                             }
                             pclose(fp);
                         }
                     }
                 }
-                else
-                {
+                else {
                     char msg[256];
-                    snprintf(msg, sizeof(msg), "Unknown command in .hub_cmd: %s\n", command);
-                    write(pipe_fd, msg, strlen(msg));
+                    snprintf(msg, sizeof(msg), "Unknown command: %.230s\n", command); // limit to avoid buffer overflow
+                    write(STDOUT_FILENO, msg, strlen(msg));
                 }
             }
 
             fclose(cmd_file);
         }
-
-        usleep(100000);
+        usleep(100000); 
     }
 
     printf("Monitor is shutting down...\n");
@@ -176,10 +137,8 @@ void monitor_loop()
     exit(0);
 }
 
-int main()
-{
+int main() {
     struct sigaction sa1, sa2;
-
     sa1.sa_handler = handle_sigusr1;
     sigemptyset(&sa1.sa_mask);
     sa1.sa_flags = 0;
--- treasure_hub.c
+++ treasure_hub.c
@@ -5,23 +5,25 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <dirent.h>
 
 pid_t monitor_pid = -1;
 int monitor_exited = 0;
 
-void sigchld_handler(int sig)
-{
+void sigchld_handler(int sig) {
     int status;
-    waitpid(monitor_pid, &status, WNOHANG);
-    monitor_exited = 1;
+    pid_t pid;
+
+    // Reap all terminated children
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        if (pid == monitor_pid) {
+            monitor_exited = 1;
+        }
+    }
 }
 
-void write_command(const char *command)
-{
+void write_command(const char *command) {
     int fd = open(".hub_cmd", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1)
-    {
+    if (fd == -1) {
         perror("Cannot open .hub_cmd");
         return;
     }
@@ -30,20 +32,16 @@ void write_command(const char *command)
     close(fd);
 }
 
-void strip_newline(char *str)
-{
-    for (int i = 0; str[i]; ++i)
-    {
-        if (str[i] == '\n')
-        {
+void strip_newline(char *str) {
+    for (int i = 0; str[i]; ++i) {
+        if (str[i] == '\n') {
             str[i] = '\0';
             break;
         }
     }
 }
 
-int main()
-{
+int main() {
     struct sigaction sa;
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
@@ -52,90 +50,83 @@ int main()
 
     char input[256];
 
-    while (1)
-    {
+    while (1) {
         printf("> ");
         fflush(stdout);
-        if (!fgets(input, sizeof(input), stdin))
-            break;
+        if (!fgets(input, sizeof(input), stdin)) break;
 
         strip_newline(input);
 
-        if (strcmp(input, "start_monitor") == 0)
-        {
-            if (monitor_pid != -1)
-            {
+        if (strcmp(input, "start_monitor") == 0) {
+            if (monitor_pid != -1 && !monitor_exited) {
                 printf("Monitor already running.\n");
                 continue;
             }
             monitor_pid = fork();
-            if (monitor_pid == 0)
-            {
+            if (monitor_pid == 0) {
                 execl("./monitor", "./monitor", NULL);
                 perror("Failed to exec monitor");
                 exit(1);
-            }
-            else
-            {
+            } else {
+                monitor_exited = 0;
                 printf("Started monitor (PID: %d)\n", monitor_pid);
             }
         }
-        else if (strcmp(input, "stop_monitor") == 0)
-        {
-            if (monitor_pid == -1)
-            {
+        else if (strcmp(input, "stop_monitor") == 0) {
+            if (monitor_pid == -1 || monitor_exited) {
                 printf("No monitor running.\n");
                 continue;
             }
             kill(monitor_pid, SIGUSR2);
             printf("Sent termination signal to monitor.\n");
         }
-        else if (strcmp(input, "exit") == 0)
-        {
-            if (monitor_pid != -1 && !monitor_exited)
-            {
+        else if (strcmp(input, "exit") == 0) {
+            if (monitor_pid != -1 && !monitor_exited) {
                 printf("Monitor still running! Stop it first.\n");
-            }
-            else
-            {
+            } else {
                 break;
             }
         }
         else if (strncmp(input, "list_hunts", 10) == 0 ||
                  strncmp(input, "list_treasures", 14) == 0 ||
                  strncmp(input, "view_treasure", 13) == 0 ||
-                 strncmp(input,"calculate_score", 15) == 0)
-        {
-            if (monitor_pid == -1 || monitor_exited)
-            {
+                 strncmp(input, "calculate_score", 15) == 0) {
+
+            if (monitor_pid == -1 || monitor_exited) {
                 printf("No active monitor.\n");
                 continue;
             }
+
             int pipefd[2];
-            pipe(pipefd);
-
-            FILE *pf = fopen(".pipe_fd", "w");
-            if (pf) {
-                fprintf(pf, "%d\n", pipefd[1]); 
-                fclose(pf);
+            if (pipe(pipefd) == -1) {
+                perror("pipe");
+                continue;
             }
 
-            write_command(input);
-            kill(monitor_pid, SIGUSR1);
+            pid_t helper = fork();
+            if (helper == 0) {
+                // CHILD: redirect STDOUT to write-end of pipe
+                close(pipefd[0]);
+                dup2(pipefd[1], STDOUT_FILENO);
+                close(pipefd[1]);
 
-            char buffer[256];
-            int bytes;
-            while ((bytes = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
-                buffer[bytes] = '\0';
-                printf("%s", buffer);
+                write_command(input);
+                kill(monitor_pid, SIGUSR1);
+                exit(0);
+            } else {
+                // PARENT: read from read-end
+                close(pipefd[1]);
+                char buffer[256];
+                int bytes;
+                while ((bytes = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
+                    buffer[bytes] = '\0';
+                    printf("%s", buffer);
+                }
+                close(pipefd[0]);
+                waitpid(helper, NULL, 0);
             }
-
-            close(pipefd[0]);
-            close(pipefd[1]);
-        
         }
-        else
-        {
+        else {
             printf("Unknown command.\n");
         }
     }
